dfe3c740e4e5c20c9910340e234199e9c2198cd7
matenboi <mjrahbar2005@gmail.com>
2024-10-10 22:42:17 -0700

    Followed a YT tutorial re: 2d Animation
    https://www.youtube.com/watch?v=Gf8LOFNnils&list=PLgOEwFbvGm5o8hayFB6skAfa8Z-mw4dPV&index=2
    
    I watched and followed along the link above, to get a feel for what the animation process would be like, once we import the assets.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,37 +8,58 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     private Rigidbody2D body;
+    private Animator animator;
+    private bool isGrounded;
 
     // Is called when script instance is loaded
     private void Awake()
     {
         // Script will check the Player Object has as RigidBody2D
         body = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
         
+        // Gets Animator component
+        animator = GetComponent<Animator>();
     }
-
+    
     // Update is called once per frame
     private void Update()
     {
-        // Left and Right inputs ONLY
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, body.velocity.y);
-
-        // Uses whatever unity has set as "Jump" as the input GEtIn
-        if (Input.GetButtonDown("Jump"))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
+        
+        // Ensures that if the Player is facing to the Right,
+        // the sprite is facing Right
+        if (horizontalInput > 0.01f)
         {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            transform.localScale = new Vector3(1, 1, 1);
         }
 
-        /*
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // Ensures that if the Player is facing to the Left,
+        // the sprite is flipped (facing left)
+        if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Uses whatever unity has set as "Jump" as the input
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded)
+            Jump();
+        
+        // Set animator parameters
+        animator.SetBool("Running", horizontalInput != 0);
+        animator.SetBool("Grounded", isGrounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
+        animator.SetTrigger("Jump");
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
         {
-            body.velocity = new Vector2(body.velocity.x, -jumpForce);
+            isGrounded = true;
         }
-        */
     }
 }
